2f2311e95ef478329ff10e28ad25282d434d698f
anels28 <55111942+anels28@users.noreply.github.com>
2019-10-02 22:29:33 -0400

    image, index file

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <title> Home | Assignment 4: Odd Student Out</title>
+   </head>
+
+   <body>
+
+      <header>
+         <a href="index.php">
+         <img href="index.php" src="images/computer.png" alt="computer">
+         </a>
+      </header>
+
+      <nav>
+         <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="kat/kat.php">Kat Fuss</a></li>
+            <li><a href="jen/jen.php">Jennifer Ngene</a></li>
+            <li><a href="dewey/dewey.php">Dewey Bazirake</a></li>
+         </ul>
+      </nav>
+
+      <main>
+         <h1>Odd Student Out: Two DMS, One CS</h1>
+         <h2>We have two DMS majors in Kat and Jennifer. We have one CS major in Dewey. What makes them different?</h2>
+         <p>The University of Rochester has both majors. Our DMS major focuses on an interdisciplinary approach to the liberal arts, a tradition of expertise in imagining and entrepreneurship, and a University-wide commitment to supporting digital technology and the arts. Our CS major focuses on giving students a solid and rigorous background in computer science principles including the requisite mathematical foundations and to expose students to the problems and solution techniques used in the various areas of the discipline.</p>
+
+         <p>Kat believes her major is important as the "world becomes more digitized and driven by technology." Jennifer finds her major fascinating because it is the "composition of turning the ideas that she has in her head into digital pieces." Dewey thinks his major allows people to "work holistically as humans to advance in technology." Learn more about them by clicking on their names.</p>
+
+         <!--Citation: Rochester DMS Program: https://www.sas.rochester.edu/dms/undergraduate/index.html
+         Citation: Rochester CS Program: https://www.cs.rochester.edu/undergraduate/index.html-->
+      </main>
+
+      <footer>CSC 174: Advanced Front-end Web Design and Development</footer>
+
+   </body>
+
+</html>
---
+++ readme.md
@@ -0,0 +1,15 @@
+* Team Name
+Quito City Team
+
+* Team Roles
+Alyssa Nelson- IA
+Clara Martinez Rubio- Visual Designer
+Maria Mancheno- Technical Coder
+
+* Link to Website
+Link to the team’s website on the class webserver which won’t be available immediately but will exist eventually. (You need to add this before you turn-in the assignment on Wednesday.)
+
+
+* A short description of the website’s domain
+
+* A short description of the website’s information architecture in terms of its: ontology, taxonomy, and choreography
